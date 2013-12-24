fc2ff5351739fe9b918532f4a16291d08b1f2cd6
John Tyree <jtyree@enthought.com>
2013-12-24 10:38:22 -0600

    Fill out setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,20 @@ from setuptools import setup
 
 setup(name='rio',
       version='0.0.1',
-      py_modules=['rio'],
+      packages=['rio'],
+      author='John Tyree',
+      author_email='johntyree@gmail.com',
+      license='GPLv3+',
+      url='http://github.com/johntyree/rio',
+      description="Stream ShoutCast stations, skipping ad content.",
+      keywords="shoutcast radio",
+      long_description=open('README.md').read(),
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "License :: OSI Approved :: "
+          "GNU General Public License v3 or later (GPLv3+)",
+          "Topic :: Multimedia :: Sound/Audio :: Analysis",
+          "Topic :: Multimedia :: Sound/Audio :: Capture/Recording",
+          "Topic :: Utilities",
+      ],
       )
