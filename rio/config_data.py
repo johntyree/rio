f19c4464c2075d456906e68266d4e2b7f8f3cc6f
John Tyree <jtyree@enthought.com>
2013-12-26 17:24:53 -0600

    Add python-syntax config file

---
+++ rio/config_data.py
@@ -0,0 +1,129 @@
+{
+    "stream": {
+        "di_psybient": {
+            "url": "http://pub1.di.fm/di_psybient",
+            "network": ["difm"],
+        },
+        "di_ambient": {
+            "url": "http://pub1.di.fm/di_ambient",
+            "network": ["difm"],
+        },
+        "di_spacedreams": {
+            "url": "http://pub1.di.fm/di_spacedreams",
+            "network": ["difm"],
+        },
+        "di_chillout": {
+            "url": "http://pub1.di.fm/di_chillout",
+            "network": ["difm"],
+        },
+        "aair-lounge-radio": {
+            "url": "http://listen.radionomy.com/aair-lounge-radio",
+            "network": ["radionomy"],
+        },
+        "air-lounge": {
+            "url": "http://listen.radionomy.com/air-lounge",
+            "network": ["radionomy"],
+        },
+        "beatlounge radio": {
+            "url": "http://206.217.201.137:8059",
+            "network": ["beatlounge radio"],
+        },
+        "di_lounge": {
+            "url": "http://pub1.di.fm/di_lounge",
+            "network": ["difm"],
+        },
+        "di_harddance": {
+            "url": "http://pub1.di.fm/di_harddance",
+            "network": ["difm"],
+        },
+        "di_hardstyle": {
+            "url": "http://pub1.di.fm/di_hardstyle",
+            "network": ["difm"],
+        },
+        "di_handsup": {
+            "url": "http://pub1.di.fm/di_handsup",
+            "network": ["difm"],
+        },
+        "di_hardcore": {
+            "url": "http://pub1.di.fm/di_hardcore",
+            "network": ["difm"],
+        },
+        "di_discohouse": {
+            "url": "http://pub1.di.fm/di_discohouse",
+            "network": ["difm"],
+        },
+        "di_funkyhouse": {
+            "url": "http://pub1.di.fm/di_funkyhouse",
+            "network": ["difm"],
+        },
+        "atr 90s dance": {
+            "url": "http://wms-13.streamsrus.com:13930",
+            "network": ["addictedtoradio"],
+        },
+        "aqua-radio-online": {
+            "url": "http://listen.radionomy.com/aqua-radio-online",
+            "network": ["radionomy"],
+        },
+        "peripou-web-radio": {
+            "url": "http://listen.radionomy.com/peripou-web-radio",
+            "network": ["radionomy"],
+        },
+    },
+
+    # This file is ast.literal_eval'd,
+    # which assumes all strings are raw.
+    "ad": {
+        "somafm": [
+            "Please Donate to support",
+            "SomaFM - Christmas Lounge Id",
+        ],
+        "beatlounge radio": [
+            # "ID - BeatLounge Radio dot com",  # Short, not bad
+        ],
+        "addictedtoradio": [
+        ],
+        "radionomy": [
+            "Radionomy - Radionomy",
+            "Tte l'équipe vous souhaite un - Jingles",
+            "Un Noel plein de musique - Jingles",
+            "Air Lounge Radio - Jingle ?\d*",
+            "Musicplus - Jingle intro ?\d*$",
+            "Sfx - AdArrival",
+            "AddictedToRadio",
+        ],
+        "difm": [
+            "ADWTAG",
+            "www\.di\.fm/jobs",
+            "Choose premium for the best audio experience",
+            "There's more to Digitally Imported!",
+        ],
+    },
+
+    "genre": {
+        "dance": [
+            "di_discohouse",
+            "di_funkyhouse",
+            "atr 90s dance",
+            "aqua-radio-online",
+            "peripou-web-radio",
+        ],
+        "pound": [
+            "di_harddance",
+            "di_hardstyle",
+            "di_handsup",
+            "di_hardcore",
+        ],
+        "lounge": [
+            "beatlounge radio",
+            "aair-lounge-radio",
+            "air-lounge",
+            "di_lounge",
+        ],
+        "ambient": [
+            "di_psybient",
+            "di_ambient",
+            "di_spacedreams",
+            "di_chillout",
+        ],
+    },
+}
