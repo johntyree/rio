998a521f0deacfe974fd36a2a5542fdf5aeecf58
John Tyree <jtyree@enthought.com>
2014-01-06 14:59:27 -0600

    Handle list_{streams,genres} by printing relevant info and exiting

--- rio/rio.py
+++ rio/rio.py
@@ -3,15 +3,32 @@
 
 from .config import RioConfig
 from .server import serve_on_port
+from .utilities import render_dict
+
+
+def list_genres(config):
+    genres = config._config['genre']
+    print(render_dict(genres))
+
+
+def list_streams(config):
+    for stream in config.all_streams:
+        print(stream)
 
 
 def main():
     """Run main."""
-    config = RioConfig()
-    port = config.port
-    host = config.host
 
-    serve_on_port(host, port)
+    config = RioConfig()
+
+    if config.list_genres:
+        list_genres(config)
+    elif config.list_streams:
+        list_streams(config)
+    else:
+        port = config.port
+        host = config.host
+        serve_on_port(host, port)
 
     return 0
 
